227852129ffcaa3f8000f28258d61e2c61657032
TheSmileyDroid <ghsilha@gmail.com>
2023-05-28 20:30:30 -0300

    another fix in symmetric

--- src/functions/operations.py
+++ src/functions/operations.py
@@ -33,12 +33,17 @@ def symmetric_difference(graph1: Graph, graph2: Graph) -> Graph:
         graph2.get_edges()
     )
 
+    edges_for_removal = set()
+
     for edge in symmetric_difference_graph._edges:
         if (
             edge[0] not in symmetric_difference_graph._vertices
             or edge[1] not in symmetric_difference_graph._vertices
         ):
-            symmetric_difference_graph._edges.remove(edge)
+            edges_for_removal.add(edge)
+
+    for edge in edges_for_removal:
+        symmetric_difference_graph._edges.remove(edge)
 
     return symmetric_difference_graph
 
