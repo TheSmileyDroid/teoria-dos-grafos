33891ae91cda935937080b6d0abcc9d7183779b1
TheSmileyDroid <ghsilha@gmail.com>
2023-06-04 18:43:19 -0300

    fix grammar error

---
+++ src/functions/hamilton.py
@@ -0,0 +1,25 @@
+from src.types.graph import Graph
+
+
+def has_hamiltonian_cycle(graph: Graph):
+    visited = set()
+
+    def dfs(vertex: str, start: str):
+        visited.add(vertex)
+        if len(visited) == len(graph.get_vertices()):
+            if start in graph.get_neighbors(vertex):
+                return True
+        else:
+            for neighbor in graph.get_neighbors(vertex):
+                if neighbor not in visited:
+                    if dfs(neighbor, start):
+                        return True
+        visited.remove(vertex)
+        return False
+
+    for vertex in graph.get_vertices():
+        visited.clear()
+        if dfs(vertex, vertex):
+            return True
+
+    return False
